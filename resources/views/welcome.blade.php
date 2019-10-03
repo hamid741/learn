60efa9f898c5c3f11dbbe7317f3b4ae669dfc9f5
Hamid <hamidpk147@gmail.com>
2019-10-03 10:53:44 +0100

    I changed saad with hamid

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">I am learning Laravel with Saad</div>
+                <div class="title">I am learning Laravel with Hamid</div>
             </div>
         </div>
     </body>
